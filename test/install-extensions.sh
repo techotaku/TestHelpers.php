e00bc19c04ccad16631597e7f94a7198859e5a1d
Ian Li <i@techotaku.net>
2013-08-27 21:52:34 +0800

    Clean up ./test/install-extensions.sh

--- test/install-extensions.sh
+++ test/install-extensions.sh
@@ -1,5 +1,3 @@
-echo '' > ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini
-cd ..
 git clone --branch=master git://github.com/php-test-helpers/php-test-helpers.git php-test-helpers
 cd php-test-helpers
 phpize
@@ -7,7 +5,6 @@ phpize
 make
 res=`sudo make install`
 echo "zend_extension =${res##*Installing shared extensions:    }test_helpers.so"
-echo "zend_extension =${res##*Installing shared extensions:    }xdebug.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-echo "zend_extension =${res##*Installing shared extensions:    }test_helpers.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-cd ../TestHelpers.php
+echo "zend_extension =${res##*Installing shared extensions:    }test_helpers.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini
+cd ..
 php --version
