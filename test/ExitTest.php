1142241aa0bf7fc3f1be641366fcd47b8d545490
Ian Li <i@techotaku.net>
2013-08-27 10:45:52 +0800

    Add ExitTestHelper.php

---
+++ test/ExitTest.php
@@ -0,0 +1,23 @@
+<?php
+require __DIR__ . '/../src/ExitTestHelper.php';
+
+class ExitTest extends PHPUnit_Framework_TestCase {
+
+  public function testAcceptance() {
+    ExitTestHelper::init();
+    exit('exit message');
+    $this->assertEquals('exit message', ExitTestHelper::getFirstExitOutput(), 'Output verification fail!');
+    ExitTestHelper::clean();
+  }
+
+  public function testMultipleExit() {
+    ExitTestHelper::init();
+    exit('exit message 1');
+    exit('second exit');
+    exit('another exit');
+    $this->assertEquals('exit message 1', ExitTestHelper::getFirstExitOutput(), 'Output verification fail!');
+    ExitTestHelper::clean();
+  }
+
+}
+?>
