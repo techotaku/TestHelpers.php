00229dd346d70f8b40c03301ee1539e42681479d
Ian Li <i@techotaku.net>
2013-08-27 14:10:35 +0800

    Add 2 tests. Fix typo. Add comments.

--- src/ExitTestHelper.php
+++ src/ExitTestHelper.php
@@ -1,39 +1,74 @@
 <?php
+/**
+ * PHP Test Helpers
+ * 
+ * @author     Ian Li <i@techotaku.net>
+ * @copyright  Ian Li <i@techotaku.net>, All rights reserved.
+ * @license    MIT License
+ */
 
-class ExitTestHelper {
-  private static $have_exit;
-  private static $first_exit_output;
+  /**
+   * PHP exit() test helper static class
+   */
+  class ExitTestHelper {
+    private static $have_exit;
+    private static $first_exit_output;
 
-  public static function init() {
-    self::$have_exit = FALSE;
-    self::$first_exit_output = NULL;
-    set_exit_overload('ExitTestHelper::exitHander');
-  }
-
-  public static function clean() {
-    unset_exit_overload();
-    self::$have_exit = FALSE;
-    self::$first_exit_output = NULL;
-  }
-
-  public static function isThereExit() {
-    return self::$have_exit;
-  }
-
-  public static function getFirstExitOutput() {
-    return self::$first_exit_output;
-  }
-
-  private static function exitHander($param = NULL) {  
-    if (!(self::$have_exit)) {
-      self::$have_exit = TRUE;
-      echo $param ?: '';
-      self::$first_exit_output = ob_get_contents();
-      ob_end_clean();
-      ob_start();
+    /**
+     * Initialize.
+     * Register exit handler.
+     */
+    public static function init() {
+      self::$have_exit = FALSE;
+      self::$first_exit_output = NULL;
+      set_exit_overload('ExitTestHelper::exitHandler');
     }
-    return FALSE;
-  }
 
-}
+    /**
+     * Clean up.
+     * Unregister exit handler.
+     */
+    public static function clean() {
+      unset_exit_overload();
+      self::$have_exit = FALSE;
+      self::$first_exit_output = NULL;
+    }
+
+    /**
+     * Returns a value indicating whether there is any exit() was invoked.
+     *
+     * @return boolean
+     */
+    public static function isThereExit() {
+      return self::$have_exit;
+    }
+
+    /**
+     * Returns the output after first exit() was invoked.
+     *
+     * @return string
+     */
+    public static function getFirstExitOutput() {
+      return self::$first_exit_output;
+    }
+
+    /**
+     * Php exit() handler.
+     * Make it private, to prevent invoking outside.
+     *
+     * @param string $param (Optional) Exit message.
+     */
+    private static function exitHandler($param = NULL) {  
+      if (!(self::$have_exit)) {
+        self::$have_exit = TRUE;
+        echo $param ?: '';
+        self::$first_exit_output = ob_get_contents();
+        // Prevent output to STDOUT, it will help you to make your PHPUnit output clear.
+        ob_end_clean();
+        ob_start();
+      }
+      return FALSE;
+    }
+
+  }
 ?>
--- test/ExitTest.php
+++ test/ExitTest.php
@@ -1,31 +1,46 @@
 <?php
-require __DIR__ . '/../src/ExitTestHelper.php';
 
-class ExitTest extends PHPUnit_Framework_TestCase {
+  require __DIR__ . '/../src/ExitTestHelper.php';
 
-  public function testAcceptance() {
-    ExitTestHelper::init();
-    exit('exit message');
-    $this->assertEquals('exit message', ExitTestHelper::getFirstExitOutput(), 'Output verification fail!');
-    ExitTestHelper::clean();
+  class ExitTest extends PHPUnit_Framework_TestCase {
+
+    public function testAcceptance() {
+      ExitTestHelper::init();
+      exit('exit message');
+      $this->assertEquals('exit message', ExitTestHelper::getFirstExitOutput(), 'Output verification fail!');
+      ExitTestHelper::clean();
+    }
+
+    public function testAcceptanceIsTrue() {
+      ExitTestHelper::init();
+      exit('exit message');
+      $this->assertTrue(ExitTestHelper::isThereExit(), 'There should be an exit() was invoked.');
+      ExitTestHelper::clean();
+    }
+
+    public function testAcceptanceIsFalse() {
+      ExitTestHelper::init();
+      $var = 'normal action';
+      $this->assertFalse(ExitTestHelper::isThereExit(), "There shouldn't be any exit() was invoked.");
+      ExitTestHelper::clean();
+    }
+
+    public function testExitAfterPrint() {
+      ExitTestHelper::init();
+      print('normal message.');
+      exit('exit message');
+      $this->assertEquals('normal message.exit message', ExitTestHelper::getFirstExitOutput(), 'Output verification fail!');
+      ExitTestHelper::clean();
+    }
+
+    public function testMultipleExit() {
+      ExitTestHelper::init();
+      exit('exit message 1');
+      exit('second exit');
+      exit('another exit');
+      $this->assertEquals('exit message 1', ExitTestHelper::getFirstExitOutput(), 'Output verification fail!');
+      ExitTestHelper::clean();
+    }
+
   }
-
-  public function testExitAfterPrint() {
-    ExitTestHelper::init();
-    print('normal message.');
-    exit('exit message');
-    $this->assertEquals('normal message.exit message', ExitTestHelper::getFirstExitOutput(), 'Output verification fail!');
-    ExitTestHelper::clean();
-  }
-
-  public function testMultipleExit() {
-    ExitTestHelper::init();
-    exit('exit message 1');
-    exit('second exit');
-    exit('another exit');
-    $this->assertEquals('exit message 1', ExitTestHelper::getFirstExitOutput(), 'Output verification fail!');
-    ExitTestHelper::clean();
-  }
-
-}
 ?>
